a42802e2239ac8b049617bca73f2c1074ba0c646
vZireael <75550025+vZireael@users.noreply.github.com>
2021-06-22 14:11:47 -0400

    skeleton for login and registration

---
+++ vaccine-hub-api/models/user.js
@@ -0,0 +1,30 @@
+const { UnauthorizedError } = require("../utils/errors");
+
+class User {
+  static async login(credentials) {
+    // user should submit their email and password
+    // if any of these fields are missing throw error
+    //
+    // look up user in db by email
+    // if a user is found, compare the submitted password
+    // to the password in the db
+    // if there is a match, return the user
+    //
+    // if any of this goes wrong, throw an error
+    throw new UnauthorizedError("Invalid email/password combination");
+  }
+
+  static async register(credentials) {
+    // user should submit their email and password
+    // if any of those are missing, throw error
+    //
+    // make sure no user already exists in system
+    // with that same email. If so, throw error
+    //
+    // take the users password and hash it
+    // take the users email and lowercase it
+    //
+    // create a new user in the db with all their info
+    // return the user
+  }
+}
---
+++ vaccine-hub-api/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { NotFoundError } = require("./utils/errors");
+
+const app = express();
+
+// enable cross-origin resource sharing for all origins
+app.use(cors());
+// parse incoming request bodies with JSON payloads
+app.use(express.json());
+// log request info
+app.use(morgan("tiny"));
+
+// handle not found error if the endpoint doesn't match any of the endpoints in our app.
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+// generic error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on https://localhost:${PORT}`);
+});
