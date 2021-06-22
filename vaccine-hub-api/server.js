901c2e9c00fc26f2a46e13f589961efc7ec126f8
vZireael <75550025+vZireael@users.noreply.github.com>
2021-06-22 16:31:41 -0400

    set up environment variables

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { PORT } = require("./config");
 
 const { NotFoundError } = require("./utils/errors");
 
@@ -27,8 +28,6 @@ app.use((err, req, res, next) => {
     error: { message, status },
   });
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on https://localhost:${PORT}`);
